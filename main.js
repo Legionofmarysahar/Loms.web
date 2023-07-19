21d02a3090729a33d26fd4e3fb32285c63e74327
legionofmarysahar@gmail.com <legionofmarysahar@gmail.com>
2023-07-19 07:58:58 +0530

    7:58-19jul

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// Preloader
+// $(document).ready(function(){
+// 	$('div#loading').removeAttr('id');
+// });
+var preloader = document.getElementById("loading");
+// window.addEventListener('load', function(){
+// 	preloader.style.display = 'none';
+// 	})
+
+function myFunction() {
+  preloader.style.display = 'none';
+};
+
 // Hamburger menu - mobile
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -14,4 +27,16 @@ function mobileMenu() {
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+}
+
+// navbar hide
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-125px";
+  }
+  prevScrollpos = currentScrollPos;
 }
